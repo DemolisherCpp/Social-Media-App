3a6fa3845da8bbd0e296864ecdef874112215e19
DemolisherCpp <m.omerbinfarooq@gmail.com>
2024-05-04 13:13:23 +0500

    upgrading sfml

--- Social Media App/Main Code.cpp
+++ Social Media App/Main Code.cpp
@@ -1,81 +1,79 @@
 #include <iostream>
+//#include <Drawer.h>
 #include <SFML/Graphics.hpp>
 
+class texter {
+	sf::Text* text;
+	sf::Font* font;
+public:
+	texter(std::string,std::string);
+	~texter();
+	sf::Text data();
 
+};
 
-sf::Font fontloader(std::string addpath= "Jersey Sharp.ttf") {
-	std::string path="Resources\\Fonts\\";
+texter::texter(std::string addpath = "Jersey Sharp.ttf",std::string message = "No Name") {
+	std::string path = "Resources\\Fonts\\";
 	path += addpath;
-	sf::Font font;
-	if (!font.loadFromFile(path)) {
+
+
+	font = new sf::Font;
+	if (!font->loadFromFile(path)) {
 		std::cout << "The font of one of the texts could not be loaded" << std::endl;
 		exit(0);
 	}
-	return font;
+
+
+	text = new sf::Text;
+	text->setFont(*font);
+
+	text->setString(message);
+
+}
+
+texter::~texter() {
+	delete text;
+	delete font;
+}
+
+sf::Text texter:: data() {
+	return *text;
 }
 
 
-	int main() {
-		//std::cout << sf::VideoMode::getDesktopMode().width;
-		//std::cout << sf::VideoMode::getDesktopMode().height;
-		//std::cout << 0.5625 * sf::VideoMode::getDesktopMode().height;
+
+int main() {
+	//std::cout << sf::VideoMode::getDesktopMode().width;
+	//std::cout << sf::VideoMode::getDesktopMode().height;
+	//std::cout << 0.5625 * sf::VideoMode::getDesktopMode().height;
 
 
-		//test area
+	//test area
 
 
-		sf::RenderWindow Application(sf::VideoMode(0.5625 * sf::VideoMode::getDesktopMode().height, sf::VideoMode::getDesktopMode().height), "Omernator", sf::Style::Titlebar | sf::Style::Close);
+	sf::RenderWindow Application(sf::VideoMode(0.5625 * sf::VideoMode::getDesktopMode().height, sf::VideoMode::getDesktopMode().height), "Omernator", sf::Style::Titlebar | sf::Style::Close);
 
 
-		while (Application.isOpen()) {
-			sf::Event Platform;
-			while (Application.pollEvent(Platform)) {
-				if (Platform.type == Platform.Closed) {
-					Application.close();
-				}
+	while (Application.isOpen()) {
+		sf::Event Platform;
+		while (Application.pollEvent(Platform)) {
+			if (Platform.type == Platform.Closed) {
+				Application.close();
 			}
-
-			//sf::Font font;
-			//font.loadFromFile("Resources\\Fonts\\Jersey Sharp.ttf");
-			//test.setFont(font);
-			//test.setString("test");
-			//texter(lol, "Jersey Sharp.ttf");
-			Application.clear();
-			//Application.draw(test);
-			//sf::Text lol("Test", fontloader(), 30);
-			//Application.draw(lol);
-			Application.display();
-			std::cout << "test";
-			
 		}
 
 
+		//Application.clear();
+		texter test;
+		Application.draw(test.data());
+		Application.display();
+		//std::cout << "test";
 
-
-
-
-		sf::RenderWindow test(sf::VideoMode(0.5625 * sf::VideoMode::getDesktopMode().height, sf::VideoMode::getDesktopMode().height), "Omernator", sf::Style::Titlebar | sf::Style::Close);
-
-
-		while (test.isOpen()) {
-			sf::Event Platform;
-			while (test.pollEvent(Platform)) {
-				if (Platform.type == Platform.Closed) {
-					test.close();
-				}
-			}
-
-			//sf::Font font;
-			//font.loadFromFile("Resources\\Fonts\\Jersey Sharp.ttf");
-			//test.setFont(font);
-			//test.setString("test");
-			//texter(lol, "Jersey Sharp.ttf");
-			test.clear();
-			//Application.draw(test);
-			//sf::Text lol("Test", fontloader(), 30);
-			//Application.draw(lol);
-			test.display();
-			std::cout << "test";
-
-		}
 	}
+
+}
+
+
+
+
+
